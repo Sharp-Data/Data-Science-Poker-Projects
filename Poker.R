7b2906580d683a4acebb8f4ad04bd8d8cc6e2b9d
Daniel Poston <daniel@sharpdata.co>
2017-06-12 11:44:13 -0700

    Added Files

---
+++ Poker.R
@@ -0,0 +1,7 @@
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+poker_df <- read.csv(file = "C:/Users/GamingFoSho/Documents/Data-Science-Poker-Projects/Report.csv")
+head(poker_df)
+str(poker_df)
